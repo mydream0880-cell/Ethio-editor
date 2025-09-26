6e5b4ed262c8a00a9524dd32493c514d9e536903
mydream0880-cell <my.dream0880@gmail.com>
2025-09-27 01:09:27 +0300

    Create Firebase.js

---
+++ Firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDDgQT6W2OULkePu9KMlfc04u8v2We40OI",
+  authDomain: "ethio-editor.firebaseapp.com",
+  projectId: "ethio-editor",
+  storageBucket: "ethio-editor.firebasestorage.app",
+  messagingSenderId: "480278426413",
+  appId: "1:480278426413:web:bcbae220defd44b7f819a4",
+  measurementId: "G-GF4SEYJBX3"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
