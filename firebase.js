1121dcfb44eaf43b83648751b6df12a7944fb360
mydream0880-cell <my.dream0880@gmail.com>
2025-09-28 11:57:04 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDDgQT6W2OULkePu9KMlfc04u8v2We40OI",
   authDomain: "ethio-editor.firebaseapp.com",
   projectId: "ethio-editor",
-  storageBucket: "ethio-editor.firebasestorage.app",
+  storageBucket: "ethio-editor.appspot.com",
   messagingSenderId: "480278426413",
   appId: "1:480278426413:web:bcbae220defd44b7f819a4",
   measurementId: "G-GF4SEYJBX3"
