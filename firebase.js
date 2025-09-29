05eb5a4ff335ad0c83b2873bf908ef5bcd8bda60
mydream0880-cell <my.dream0880@gmail.com>
2025-09-29 18:50:33 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,12 +3,11 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.5.0/firebase
 
 const firebaseConfig = {
   apiKey: "AIzaSyDDgQT6W2OULkePu9KMlfc04u8v2We40OI",
-  authDomain: "ethio-editor.firebaseapp.com",
-  projectId: "ethio-editor",
-  storageBucket: "ethio-editor.appspot.com",
+  authDomain: "etio-editor.firebaseapp.com",
+  projectId: "etio-editor",
+  storageBucket: "etio-editor.appspot.com",
   messagingSenderId: "480278426413",
-  appId: "1:480278426413:web:bcbae220defd44b7f819a4",
-  measurementId: "G-GF4SEYJBX3"
+  appId: "1:480278426413:web:bcbae220defd44b7f819a4"
 };
 
 const app = initializeApp(firebaseConfig);
