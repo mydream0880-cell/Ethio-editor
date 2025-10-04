379fadb8e71bc93bd8ff81b95d73757567def89d
mydream0880-cell <my.dream0880@gmail.com>
2025-10-04 15:57:49 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,9 @@
-// supabase.js
+// supabase.js - SIMPLER VERSION
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
 const supabaseUrl = 'https://yxpvnshgtrmuzjybbwwt.supabase.co'
 const supabaseKey = 'eyJhbGci0iJIUZI1NiIsInR5cCI'
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+console.log("Supabase connected!")
+export { supabase }
