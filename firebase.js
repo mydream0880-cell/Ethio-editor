219bd95e754a7b69f03ee236f614123f3466a10a
mydream0880-cell <my.dream0880@gmail.com>
2025-10-04 13:08:58 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,17 @@
-// firebase.js
+// firebase.js - UPDATED CONFIG
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
 
+// Updated Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDDgQT6W2OULkePu9KMlfc04u8v2We40OI",
   authDomain: "ethio-editor.firebaseapp.com",
   projectId: "ethio-editor",
-  storageBucket: "ethio-editor.firebasestorage.app",
+  storageBucket: "ethio-editor.appspot.com",
   messagingSenderId: "480278426413",
-  appId: "1:480278426413:web:f26a5b381993f9caf819a4",
-  measurementId: "G-N2TZPPB346"
+  appId: "1:480278426413:web:f26a5b381993f9caf819a4"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+console.log("Firebase initialized successfully!");
