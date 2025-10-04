c05269c6fa5979c39a7b4579716ab1fc3477bddf
mydream0880-cell <my.dream0880@gmail.com>
2025-10-04 12:14:58 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,16 @@
-<script type="module">
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
-  import { getDatabase } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-database.js";
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyDDgQT6W2OULkePu9KMlfc04u8v2We40OI",
-    authDomain: "ethio-editor.firebaseapp.com",
-    databaseURL: "https://ethio-editor-default-rtdb.firebaseio.com",
-    projectId: "ethio-editor",
-    storageBucket: "ethio-editor.appspot.com",
-    messagingSenderId: "480278426413",
-    appId: "1:480278426413:web:e7daa9e7ec76db50f819a4",
-    measurementId: "G-8CH4HXS95B"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyDDgQT6W2OULkePu9KMlfc04u8v2We40OI",
+  authDomain: "ethio-editor.firebaseapp.com",
+  projectId: "ethio-editor",
+  storageBucket: "ethio-editor.firebasestorage.app",
+  messagingSenderId: "480278426413",
+  appId: "1:480278426413:web:f26a5b381993f9caf819a4",
+  measurementId: "G-N2TZPPB346"
+};
 
-  const app = initializeApp(firebaseConfig);
-  const db = getDatabase(app);
-
-  window.__db = db; // Make it globally available
-</script>
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
